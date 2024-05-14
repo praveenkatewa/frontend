719baf877c2eced8db8378baf4f29485caac9a27
praveen katewa <praveenkatewa.45@gmail.com>
2024-05-14 12:26:06 +0530

    new work on array

--- js/array.js
+++ js/array.js
@@ -46,22 +46,54 @@
 // console.log(max)
 
 
-let a=[10,20,30,40,50,60,70];
-let max=0;
-let s_max=0;
-for(let i =0;i<a.length;i++ ){
-     if(max<a[i]){
-          max=a[i]
-     }
-}
+// let a=[10,20,30,40,50,60,70];
+// let max=0;
+// let s_max=0;
+// for(let i =0;i<a.length;i++ ){
+//      if(max<a[i]){
+//           max=a[i]
+//      }
+// }
 
-for(let i=0;i<a.length;i++){
-     if(s_max<a[i] && max>a[i]){
-          s_max=a[i]
-     }
+// for(let i=0;i<a.length;i++){
+//      if(s_max<a[i] && max>a[i]){
+//           s_max=a[i]
+//      }
 
-}
+// }
 
 
-console.log(s_max)
+// console.log(s_max)
 
+// let x=[100,200,300,400,500,600]
+//  let last=x.pop();
+//  let first=x.shift();
+//   x.unshift(last)
+// x.push(first)
+// console.log(x)
+// console.log(x.slice(-4))
+
+
+// let x=[100,200,300,400,500,600,700,800,900]
+// console.log(x.slice(2,7))
+// console.log(x.slice(2))
+// console.log(x.slice(7,3))
+// console.log(x.slice(-9,-7))
+// console.log(x.slice(4,9))
+// console.log(x.slice(0,5))
+
+
+let x=[100,200,300,400,500,600,700,800,900]
+// let y=x.map((item)=>{
+//      return item*2;
+// })
+// console.log("original array :",x)
+// console.log("new array: ",y)
+
+// let y=x.map((item)=>item*2)
+// console.log(y)
+
+let b=x.filter((item)=>{
+     return item > 300;
+});
+console.log(b);
