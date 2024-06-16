e81dd9fb554b3e3f777764e5395b8eda86151290
praveen katewa <praveenkatewa.45@gmail.com>
2024-06-16 23:59:46 +0530

    new work done

---
+++ js/Asynchronous/13june.js
@@ -0,0 +1,6 @@
+// fetch
+// dummyjson
+// jsonplaceholder
+// https://jsonplaceholder.typicode.com/posts
+// if promiss funtion is fullfill --> use .then
+// if promiss funtion reget --> then it use .catch
